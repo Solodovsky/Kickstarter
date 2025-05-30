fa383c9d7753cbebd04e7e9023cbffad118a99a3
Solodovsky <toxa2h@bk.ru>
2025-05-30 14:28:47 +0300

    add questions section and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,24 @@ menuIcon.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
 });
+
+window.onscroll = function () {
+  const btn = document.getElementById("scrollToTopBtn");
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
